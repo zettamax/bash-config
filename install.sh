1c42da6e4279b8937ac184e46f93e0bd7334e3d1
Max Nazarov <max@908.dp.ua>
2015-04-18 18:31:17 +0700

    Add environment source file in .profile

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ D=$_BASH_CONFIG_DIR
 
 cat $D/_real_bash_profile.sh > ~/.bash_profile
 cat $D/_real_bashrc.sh > ~/.bashrc
+cat $D/_add_profile.sh >> ~/.profile
 
 rm $D/_real_bash_profile.sh $D/_real_bashrc.sh $D/install.sh
